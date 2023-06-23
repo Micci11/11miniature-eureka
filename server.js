4a76165df2dab809471b48ce2e11c1243a8abfe1
Micci Benner <123527200+Micci11@users.noreply.github.com>
2023-06-23 19:00:29 -0400

    Add files via upload
    update

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const apiRoutes = require("./Routes/apiRoutes");
+const htmlRoutes = require("./Routes/htmlRoutes");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
